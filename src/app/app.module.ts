20b96ea6328070825aed4570c30873af07ac48e1
trevin-livele <liveletrevin6@gmail.com>
2022-03-23 12:27:44 +0300

    added boiler plate

---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LandingSectionComponent } from './landing-section/landing-section.component';
+import { BookingComponent } from './booking/booking.component';
+import { TestimonialsComponent } from './testimonials/testimonials.component';
+import { PaymentsComponent } from './payments/payments.component';
+import { ReviewsComponent } from './reviews/reviews.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    LandingSectionComponent,
+    BookingComponent,
+    TestimonialsComponent,
+    PaymentsComponent,
+    ReviewsComponent,
+    LoginComponent,
+    RegisterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
